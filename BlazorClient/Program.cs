b263ba24af1bd5ecd910cf3210ebab80a64f922b
Rodrigo <91029590+romqrocha@users.noreply.github.com>
2025-10-28 14:15:08 -0700

    Testing GitHub Actions (again)
    2nd time's the charm

--- BlazorClient/Program.cs
+++ BlazorClient/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorClient;
 
-//
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
