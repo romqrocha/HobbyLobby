bbb681bdcd456c2610575dfa92875bb801e2c438
Rodrigo <91029590+romqrocha@users.noreply.github.com>
2025-11-08 17:53:45 -0800

    Added username to registration and made it unique

--- BlazorServer/Data/ApplicationDbContext.cs
+++ BlazorServer/Data/ApplicationDbContext.cs
@@ -3,33 +3,38 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServer.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Message> Messages => Set<Message>();
     public DbSet<Hobby> Hobbies => Set<Hobby>();
 
-
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<ApplicationUser>()
+        modelBuilder
+            .Entity<ApplicationUser>()
             .HasOne(u => u.FavoriteHobby1)
             .WithMany(h => h.Favorite1Users)
             .HasForeignKey(u => u.FavoriteHobby1Id);
 
-        modelBuilder.Entity<ApplicationUser>()
+        modelBuilder
+            .Entity<ApplicationUser>()
             .HasOne(u => u.FavoriteHobby2)
             .WithMany(h => h.Favorite2Users)
             .HasForeignKey(u => u.FavoriteHobby2Id);
 
-        modelBuilder.Entity<ApplicationUser>()
+        modelBuilder
+            .Entity<ApplicationUser>()
             .HasOne(u => u.FavoriteHobby3)
             .WithMany(h => h.Favorite3Users)
             .HasForeignKey(u => u.FavoriteHobby3Id);
-        
-}
 
+        // Unique username
+        modelBuilder.Entity<ApplicationUser>().HasIndex(u => u.UserName).IsUnique();
 
-
+        // Unique BCIT email
+        modelBuilder.Entity<ApplicationUser>().HasIndex(u => u.BcitEmail).IsUnique();
+    }
 }
---
+++ BlazorServer/Data/Migrations/20251109013857_ro_1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ro_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_BcitEmail",
+                table: "AspNetUsers",
+                column: "BcitEmail",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers",
+                column: "UserName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_BcitEmail",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_UserName",
+                table: "AspNetUsers");
+        }
+    }
+}
