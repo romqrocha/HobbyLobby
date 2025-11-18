fb2ac0268cd04bb1d59eb3bf02b4163c25849f08
Rodrigo <rodrigo@vancouver4us.com>
2025-11-18 13:48:05 -0800

    Automatically apply db migrations

--- BlazorServer/Program.cs
+++ BlazorServer/Program.cs
@@ -74,4 +74,12 @@ app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+}
+
 app.Run();
