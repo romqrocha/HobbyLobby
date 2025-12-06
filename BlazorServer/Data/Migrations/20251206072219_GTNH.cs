fed4cc18df81b3bfd71c9acff7c663719b44c5d1
Rodrigo <91029590+romqrocha@users.noreply.github.com>
2025-12-05 23:46:50 -0800

    lazy admin page and new hobby to show friends

---
+++ BlazorServer/Data/Migrations/20251206072219_GTNH.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GTNH : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hobbies",
+                columns: new[] { "HobbyId", "HobbyName" },
+                values: new object[] { 425, "🧭GT: NH" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hobbies",
+                keyColumn: "HobbyId",
+                keyValue: 425);
+        }
+    }
+}
